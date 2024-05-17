65eac93aea7e0672a17c60070b4156bfffe05889
Oscar <56559647+oscar7070@users.noreply.github.com>
2024-05-17 13:56:51 +0300

    Added export API.

---
+++ RTLTypeExports.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+#ifdef RTLTYPE_EXPORTS
+#define RTLTYPE_API __declspec(dllexport)
+#else
+#define RTLTYPE_API __declspec(dllimport)
+#endif
+
+extern "C" RTLTYPE_API std::string ConvertToFixed(const std::string & text);
